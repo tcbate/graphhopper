d2a1227ee0d64b2d84b030b8b226ad085a9cb978
Thomas Bate <thomas.bate@umontreal.ca>
2025-10-25 09:48:21 -0400

    Disable a test. See if the action works and detects a change

--- core/src/test/java/com/graphhopper/util/GHUtilityThomasTest.java
+++ core/src/test/java/com/graphhopper/util/GHUtilityThomasTest.java
@@ -74,7 +74,7 @@ class GHUtilityThomasTest {
         assertThrows(IllegalArgumentException.class, () -> GHUtility.getCommonNode(bg,2,3));
     }
 
-    @Test
+    @Disabled
     public void FindCommonNodeCycle() {
         assertThrows(IllegalArgumentException.class, () -> GHUtility.getCommonNode(bg,4,3));
     }
